761bba83f761904fc25518aa6845b213740a082a
Matt <Geczy@users.noreply.github.com>
2022-07-08 19:00:10 -0500

    display the calendar on the event settings page

--- apps/web/pages/event-types/index.tsx
+++ apps/web/pages/event-types/index.tsx
@@ -13,14 +13,21 @@ import {
   UsersIcon,
 } from "@heroicons/react/solid";
 import { UserPlan } from "@prisma/client";
+import { GetServerSidePropsContext, GetStaticPaths, GetStaticPropsContext } from "next";
 import { Trans } from "next-i18next";
 import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment, useState } from "react";
+import { useEffect } from "react";
+import { JSONObject } from "superjson/dist/types";
+import { z } from "zod";
 
+import { locationHiddenFilter, LocationObject } from "@calcom/app-store/locations";
 import { CAL_URL, WEBAPP_URL } from "@calcom/lib/constants";
+import { getDefaultEvent } from "@calcom/lib/defaultEvents";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
+import { parseRecurringEvent } from "@calcom/lib/isRecurringEvent";
 import showToast from "@calcom/lib/notification";
 import { Button } from "@calcom/ui";
 import { Alert } from "@calcom/ui/Alert";
@@ -35,8 +42,10 @@ import EmptyScreen from "@calcom/ui/EmptyScreen";
 import { Tooltip } from "@calcom/ui/Tooltip";
 
 import { withQuery } from "@lib/QueryCell";
+import { getSession } from "@lib/auth";
 import classNames from "@lib/classNames";
 import { HttpError } from "@lib/core/http/error";
+import prisma from "@lib/prisma";
 import { inferQueryOutput, trpc } from "@lib/trpc";
 
 import { EmbedButton, EmbedDialog } from "@components/Embed";
@@ -48,6 +57,8 @@ import SkeletonLoader from "@components/eventtype/SkeletonLoader";
 import Avatar from "@components/ui/Avatar";
 import AvatarGroup from "@components/ui/AvatarGroup";
 import Badge from "@components/ui/Badge";
+
+import UserCal from "../[user]/[type]";
 
 type EventTypeGroups = inferQueryOutput<"viewer.eventTypes">["eventTypeGroups"];
 type EventTypeGroupProfile = EventTypeGroups[number]["profile"];
@@ -533,7 +544,7 @@ const CTA = () => {
 
 const WithQuery = withQuery(["viewer.eventTypes"]);
 
-const EventTypesPage = () => {
+function EventTypesPage(props: AvailabilityPageProps) {
   const { t } = useLocale();
   return (
     <div>
@@ -541,6 +552,7 @@ const EventTypesPage = () => {
         <title>Home | Cal.com</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <UserCal {...props} />
       <Shell
         heading={t("event_types_page_title") as string}
         subtitle={t("event_types_page_subtitle") as string}
@@ -592,6 +604,133 @@ const EventTypesPage = () => {
       </Shell>
     </div>
   );
+}
+
+async function getUserPageProps(context: GetServerSidePropsContext) {
+  const { ssgInit } = await import("@server/lib/ssg");
+  const ssg = await ssgInit(context);
+  const session = await getSession(context);
+
+  console.log(session);
+
+  if (!session?.user?.id) {
+    return { redirect: { permanent: false, destination: "/settings/profile" } };
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: session.user.id,
+    },
+    select: {
+      id: true,
+      username: true,
+      away: true,
+      plan: true,
+      name: true,
+      hideBranding: true,
+      timeZone: true,
+      theme: true,
+      weekStart: true,
+      brandColor: true,
+      darkBrandColor: true,
+      eventTypes: {
+        select: { id: true },
+        // Order by position is important to ensure that the event-type that's enabled is the first in the list because for Free user only first is allowed.
+        orderBy: [
+          {
+            position: "desc",
+          },
+          {
+            id: "asc",
+          },
+        ],
+      },
+    },
+  });
+
+  if (!user) {
+    throw new Error("User seems logged in but cannot be found in the db");
+  }
+
+  const eventTypeIds = user.eventTypes.map((e) => e.id);
+  const eventType = await prisma.eventType.findUnique({
+    where: {
+      id: eventTypeIds[0],
+    },
+    select: {
+      title: true,
+      slug: true,
+      recurringEvent: true,
+      length: true,
+      locations: true,
+      id: true,
+      description: true,
+      price: true,
+      currency: true,
+      requiresConfirmation: true,
+      schedulingType: true,
+      metadata: true,
+      seatsPerTimeSlot: true,
+      users: {
+        select: {
+          name: true,
+          username: true,
+          hideBranding: true,
+          brandColor: true,
+          darkBrandColor: true,
+          theme: true,
+          plan: true,
+          allowDynamicBooking: true,
+          timeZone: true,
+        },
+      },
+    },
+  });
+
+  if (!eventType) return { notFound: true };
+
+  const locations = eventType.locations ? (eventType.locations as LocationObject[]) : [];
+
+  const eventTypeObject = Object.assign({}, eventType, {
+    metadata: (eventType.metadata || {}) as JSONObject,
+    recurringEvent: parseRecurringEvent(eventType.recurringEvent),
+    locations: locationHiddenFilter(locations),
+    users: eventType.users.map((user) => ({
+      name: user.name,
+      username: user.username,
+      hideBranding: user.hideBranding,
+      plan: user.plan,
+      timeZone: user.timeZone,
+    })),
+  });
+
+  const profile = eventType.users[0] || user;
+
+  return {
+    props: {
+      eventType: eventTypeObject,
+      profile: {
+        theme: user.theme,
+        name: user.name,
+        username: user.username,
+        hideBranding: user.hideBranding,
+        plan: user.plan,
+        timeZone: user.timeZone,
+        weekStart: user.weekStart,
+        brandColor: user.brandColor,
+        darkBrandColor: user.darkBrandColor,
+        slug: `${profile.username}/${eventType.slug}`,
+        image: `${WEBAPP_URL}/${profile.username}/avatar.png`,
+      },
+      away: user?.away,
+      isDynamic: false,
+      trpcState: ssg.dehydrate(),
+    },
+  };
+}
+
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+  return await getUserPageProps(context);
 };
 
 export default EventTypesPage;
