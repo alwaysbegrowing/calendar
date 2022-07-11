454173c1303fe5db2da452a533583e335415399e
Matt <Geczy@users.noreply.github.com>
2022-07-11 10:02:54 -0500

    show getting started on new users when redirecting to /home

--- apps/web/pages/home.tsx
+++ apps/web/pages/home.tsx
@@ -36,8 +36,6 @@ async function getUserPageProps(context: GetServerSidePropsContext) {
   const ssg = await ssgInit(context);
   const session = await getSession(context);
 
-  console.log(session);
-
   if (!session?.user?.id) {
     return { redirect: { permanent: false, destination: "/settings/profile" } };
   }
@@ -78,58 +76,60 @@ async function getUserPageProps(context: GetServerSidePropsContext) {
   }
 
   const eventTypeIds = user.eventTypes.map((e) => e.id);
-  const eventType = await prisma.eventType.findUnique({
-    where: {
-      id: eventTypeIds[1],
-    },
-    select: {
-      title: true,
-      slug: true,
-      recurringEvent: true,
-      length: true,
-      locations: true,
-      id: true,
-      description: true,
-      price: true,
-      currency: true,
-      requiresConfirmation: true,
-      schedulingType: true,
-      metadata: true,
-      seatsPerTimeSlot: true,
-      users: {
-        select: {
-          name: true,
-          username: true,
-          hideBranding: true,
-          brandColor: true,
-          darkBrandColor: true,
-          theme: true,
-          plan: true,
-          allowDynamicBooking: true,
-          timeZone: true,
+  const eventType = eventTypeIds.length
+    ? await prisma.eventType.findUnique({
+        where: {
+          id: eventTypeIds[1],
         },
-      },
-    },
-  });
+        select: {
+          title: true,
+          slug: true,
+          recurringEvent: true,
+          length: true,
+          locations: true,
+          id: true,
+          description: true,
+          price: true,
+          currency: true,
+          requiresConfirmation: true,
+          schedulingType: true,
+          metadata: true,
+          seatsPerTimeSlot: true,
+          users: {
+            select: {
+              name: true,
+              username: true,
+              hideBranding: true,
+              brandColor: true,
+              darkBrandColor: true,
+              theme: true,
+              plan: true,
+              allowDynamicBooking: true,
+              timeZone: true,
+            },
+          },
+        },
+      })
+    : null;
 
-  if (!eventType) return { notFound: true };
+  const locations = eventType?.locations ? (eventType.locations as LocationObject[]) : [];
 
-  const locations = eventType.locations ? (eventType.locations as LocationObject[]) : [];
+  const eventTypeObject =
+    eventType &&
+    Object.assign({}, eventType, {
+      metadata: (eventType.metadata || {}) as JSONObject,
+      recurringEvent: parseRecurringEvent(eventType.recurringEvent),
+      locations: locationHiddenFilter(locations),
+      users: eventType.users.map((user) => ({
+        name: user.name,
+        username: user.username,
+        hideBranding: user.hideBranding,
+        plan: user.plan,
+        timeZone: user.timeZone,
+      })),
+    });
 
-  const eventTypeObject = Object.assign({}, eventType, {
-    metadata: (eventType.metadata || {}) as JSONObject,
-    recurringEvent: parseRecurringEvent(eventType.recurringEvent),
-    locations: locationHiddenFilter(locations),
-    users: eventType.users.map((user) => ({
-      name: user.name,
-      username: user.username,
-      hideBranding: user.hideBranding,
-      plan: user.plan,
-      timeZone: user.timeZone,
-    })),
-  });
-
-  const profile = eventType.users[0] || user;
+  const profile = eventType?.users[0] || user;
 
   return {
     props: {
@@ -144,7 +144,7 @@ async function getUserPageProps(context: GetServerSidePropsContext) {
         weekStart: user.weekStart,
         brandColor: user.brandColor,
         darkBrandColor: user.darkBrandColor,
-        slug: `${profile.username}/${eventType.slug}`,
+        slug: `${profile.username}/${eventType?.slug}`,
         image: `${WEBAPP_URL}/${profile.username}/avatar.png`,
       },
       away: user?.away,
